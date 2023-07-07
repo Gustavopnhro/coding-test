492176708dd531377e56980b26e9ec2f4a03570c
Gustavo Snts <gustavosnts.contato@gmail.com>
2023-07-07 00:21:36 -0300

    Third test  on second challenge

--- second case/challenge.py
+++ second case/challenge.py
@@ -1,10 +1,14 @@
 #input: "Hello, World"
 #output: "Helo, Wrd!"
 
-phrase = "Hello, World"
+phrase = "Hello, World!"
 splitedPhrase = []
-
+newPhrase = []
 for letter in phrase:
     splitedPhrase.append(letter)
 
-print(splitedPhrase)
+for i in range (0, len(splitedPhrase)):
+    if splitedPhrase[i] not in newPhrase :
+        newPhrase.append(splitedPhrase[i])
+    
+print(newPhrase)
