d046696ab2129bbb047be523a5b0cadd094be228
Gustavo Snts <gustavosnts.contato@gmail.com>
2023-07-07 00:54:48 -0300

    First test on third challenge

--- third case/challenge.py
+++ third case/challenge.py
@@ -1,4 +1,18 @@
 #input: "babad"
 #output: "bab"
 
-phrase = "babad"
+phrase = "rack"
+splitedPhrase = []
+palindrome = []
+
+for letter in phrase:
+    splitedPhrase.append(letter)
+
+
+for i in range(0, len(splitedPhrase)):
+    palindrome.append(splitedPhrase[i])
+    if ''.join(palindrome) == phrase[0:i+2] and len(palindrome)>2:
+        break
+
+    
+print(''.join(palindrome))
