724fa030d2308f8762e1d9e9a0407eb256e28ed5
Gustavo Snts <gustavosnts.contato@gmail.com>
2023-07-07 01:15:42 -0300

    Refactoring the code

--- third case/challenge.py
+++ third case/challenge.py
@@ -2,20 +2,15 @@
 #output: "bab"
 
 phrase = "babad"
-splitedPhrase = []
-palindrome = []
 
-for letter in phrase:
-    splitedPhrase.append(letter)
+while True:
+    splitedPhrase = []
+    palindrome = []
 
+    for letter in phrase:
+        palindrome.append(letter)
+        if ''.join(palindrome) in phrase[::-1] and len(palindrome)>2:
+            break    
+    print(''.join(palindrome))
+    phrase = str(input("Input your word \n"))
 
-for i in range(0, len(splitedPhrase)):
-    palindrome.append(splitedPhrase[i])
-    print('Phrase = '+ phrase[0:i+1] )
-    print ('Palindrome ='+''.join(palindrome))
-    if ''.join(palindrome) in phrase[::-1] and len(palindrome)>2:
-        break
-
-    
-
-print(palindrome)
