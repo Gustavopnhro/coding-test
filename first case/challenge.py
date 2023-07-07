49220c5f16a06c59f7762c591a67658231997bf3
Gustavo Snts <gustavosnts.contato@gmail.com>
2023-07-07 00:03:44 -0300

    Setting a input to interact with the user

--- first case/challenge.py
+++ first case/challenge.py
@@ -1,10 +1,9 @@
 #input: "Hello, World! OpenAI is amazing"
 #output: ".amazing is OpenAI World! Hello"
 
-
+phrase = "Hello, World! OpenAI is amazing"
 while True:
-    phrase = "Hello, World! OpenAI is amazing"
     splitPhrase = phrase.split(' ')
     splitPhrase.reverse()
     print(' '.join(splitPhrase))
-    input()
+    phrase = input(str("Insert your phrase \n"))
