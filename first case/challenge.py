cb011392d1c2c10449515aacda4c9f4378e81a4e
Gustavo Snts <gustavosnts.contato@gmail.com>
2023-07-06 23:46:44 -0300

    First test

---
+++ first case/challenge.py
@@ -0,0 +1,9 @@
+#input: "Hello, World! OpenAI is amazing"
+#output: ".amazing is OpenAI World! Hello"
+
+phrase = "Hello, World! OpenAI is amazing"
+
+
+splitPhrase = phrase.split(' ')
+
+print(splitPhrase)
