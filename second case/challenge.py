ca0dcb1b9ab40dd4d5cfe1fb16205af1de23034c
Gustavo Snts <gustavosnts.contato@gmail.com>
2023-07-07 00:24:00 -0300

    Refactoring the code

--- second case/challenge.py
+++ second case/challenge.py
@@ -5,10 +5,7 @@ phrase = "Hello, World!"
 splitedPhrase = []
 newPhrase = []
 for letter in phrase:
-    splitedPhrase.append(letter)
+    if letter not in newPhrase:
+        newPhrase.append(letter)
 
-for i in range (0, len(splitedPhrase)):
-    if splitedPhrase[i] not in newPhrase :
-        newPhrase.append(splitedPhrase[i])
-    
-print(newPhrase)
+print(''.join(newPhrase))
