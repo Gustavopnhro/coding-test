3e314a5a339c03611524a76471a6633f2a49450d
Gustavo Snts <gustavosnts.contato@gmail.com>
2023-07-07 01:11:27 -0300

    Third test on third challenge

--- third case/challenge.py
+++ third case/challenge.py
@@ -1,7 +1,7 @@
 #input: "babad"
 #output: "bab"
 
-phrase = "auto"
+phrase = "babad"
 splitedPhrase = []
 palindrome = []
 
@@ -9,15 +9,13 @@ for letter in phrase:
     splitedPhrase.append(letter)
 
 
-# for i in range(0, len(splitedPhrase)):
-#     palindrome.append(splitedPhrase[i])
-#     print('Phrase = '+ phrase[0:i+1] )
-#     print ('Palindrome ='+''.join(palindrome))
-#     if ''.join(palindrome) == phrase.__reversed__ and len(palindrome)>2:
-#         break
+for i in range(0, len(splitedPhrase)):
+    palindrome.append(splitedPhrase[i])
+    print('Phrase = '+ phrase[0:i+1] )
+    print ('Palindrome ='+''.join(palindrome))
+    if ''.join(palindrome) in phrase[::-1] and len(palindrome)>2:
+        break
 
     
-splitedPhrase.reverse()
 
-if ('auto' in ''.join(splitedPhrase)):
-    print ("achei")
+print(palindrome)
