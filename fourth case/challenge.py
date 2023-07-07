82f3f48db329a889e9f67087828f4108f4fdbfbb
Gustavo Snts <gustavosnts.contato@gmail.com>
2023-07-07 09:37:28 -0300

    First test on the fourth case

--- fourth case/challenge.py
+++ fourth case/challenge.py
@@ -2,3 +2,13 @@
 #output = "Hello. How are you? I'm fine, thank you."
 
 
+
+phrase = "hello. how are you? i'm fine, thank you."
+words = phrase.split(' ')
+
+for i, word in enumerate(words):
+    if words[i][::-1][0] in ['.', '?', '!']:
+        print('achei')
+
+
+print (words[0][::-1][0])
